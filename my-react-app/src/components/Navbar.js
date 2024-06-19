aa40221f4ebfbe37f7d2b575934aa15b93226964
Musa <royaldesire10@gmail.com>
2024-06-19 10:23:54 +0200

    saving a navbar

---
+++ my-react-app/src/components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return <nav className="nav">
+        <a href="" className="site-title">AWS Cohort</a>
+        <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/testimonial">Testimonial</a></li>
+        </ul>
+    </nav>
+}
