ebf0fe9f0240e1279a795b9e2fb98060a6601813
Musa <royaldesire10@gmail.com>
2024-06-19 13:54:43 +0200

    saving

--- my-react-app/src/pages/Testimonial.js
+++ my-react-app/src/pages/Testimonial.js
@@ -1,5 +1,116 @@
 export default function Testimonial(){
-    return <h1>
-        testimonial
-    </h1>
+    return(
+        <>
+        <section class="about" id="about">
+        <div class="container">
+            <h2>Testimonials</h2>
+            <p>Testimonials from Junior AWS Cloud Engineers: Discover Their Journeys of Learning, Achievement, and Innovation!</p>
+        </div>
+        </section>
+
+
+        <div class="testimonial-container">
+        <div class="testimonial-card">
+            <img src="images/mutshax.jpg" alt="Jane Doe" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"The program was simply outstanding, offering experiences that left an indelible mark on my personal and professional growth. It was a journey defined by innovation, collaboration, and endless opportunities for learning."</p>
+                <p class="testimonial-author">- Rembuluwani Mutsharini | Junior Solution Architect</p>
+            </div>
+        </div>
+        <div class="testimonial-card">
+            <img src="images/thobile.jpg" alt="John Smith" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"The program was extraordinary, providing unparalleled experiences that propelled me forward in both personal and professional realms. It was a journey marked by innovation, collaboration, and lasting impact."</p>
+                <p class="testimonial-author">- Thobile Twala | Junior Solutions Architect.</p>
+            </div>
+        </div>
+        <div class="testimonial-card">
+            <img src="images/musa.jpg" alt="Sarah Lee" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"The program was exceptional, delivering invaluable experiences that fueled my growth and development. It was a transformative journey, rich with opportunities for learning, collaboration, and personal enrichment."</p>
+                <p class="testimonial-author">- Mavasa Musa Royal | Junior AWS Developer.</p>
+            </div>
+        </div>
+        <div class="testimonial-card">
+            <img src="images/ginna.jpg" alt="Michael Brown" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"Being part of this cohort has been an incredible journey. The synergy among team members,
+                     coupled with the diverse projects, has created an enriching experience that's 
+                     both rewarding and inspiring. It's truly amazing how much we've achieved together, 
+                     and I'm grateful for the opportunity to be part of this exceptional cohort."</p>
+                <p class="testimonial-author">- Seerane Ginna | AWS Data Engineer.</p>
+            </div>
+        </div>
+
+        <div class="testimonial-card">
+            <img src="images/andriesmatenjwa.jpg" alt="Michael Brown" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"As a member of our cohort, 
+                    the experience has been nothing short of amazing. The collaborative environment,
+                     coupled with innovative projects, has truly elevated my journey, fostering personal 
+                     growth and professional development beyond expectation. Grateful for the opportunity to thrive 
+                    alongside such incredible talents."</p>
+                <p class="testimonial-author">- Andries Matenjwa | Junior AWS Developer .</p>
+            </div>
+        </div>
+
+        <div class="testimonial-card">
+            <img src="images/reginald.png" alt="Michael Brown" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"Joining this cohort has been an extraordinary experience. Surrounded by talented peers and engaging projects, every moment has been filled with learning, growth, and camaraderie. It's been an amazing journey, and I'm honored to have been a part of such an exceptional cohort."</p>
+                <p class="testimonial-author">- Khumalo Reginald | Junior AWS Solutions Architect / Business Development Executive.</p>
+            </div>
+        </div>
+
+        <div class="testimonial-card">
+            <img src="images/humprey.jpg" alt="Michael Brown" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"Joining this cohort was a game-changer. Surrounded by talent and innovation, every moment was a chance to learn, grow, and excel. It's been an unforgettable journey of discovery and camaraderie."</p>
+                <p class="testimonial-author">- Manaka Humprey | Junior AWS Solutions Architect / Business Development Executive..</p>
+            </div>
+        </div>
+
+        <div class="testimonial-card">
+            <img src="images/tshego.jpg" alt="Michael Brown" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"The program was truly remarkable, offering a wealth of experiences that fostered growth and learning in extraordinary ways. It was an unforgettable journey characterized by innovation, collaboration, and boundless opportunity."</p>
+                <p class="testimonial-author">- Tshegofatso Letswalo | Junior AWS Developer.</p>
+            </div>
+        </div>
+
+        <div class="testimonial-card">
+            <img src="images/kgodiso.jpg" alt="Michael Brown" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"The program was phenomenal, providing 
+                    unparalleled experiences that propelled both personal 
+                    and professional growth. It was an unforgettable 
+                    journey marked by innovation, collaboration, and 
+                    continuous learning."</p>
+                <p class="testimonial-author">- Kgodiso Mphahlele | Junior AWS Solutions Architect.</p>
+            </div>
+        </div>
+
+        <div class="testimonial-card">
+            <img src="images/zandi.jpg" alt="Michael Brown" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"The program was truly exceptional, offering an unparalleled
+                     experience filled with learning, growth, and 
+                     opportunities. From innovative projects to insightful mentorship, every aspect was 
+                     designed to inspire and challenge us. It was an incredible journey that not only 
+                     honed our skills but also fostered lifelong connections and memories."</p>
+                <p class="testimonial-author">- Zandile Mngomezulu | Junior AWS Solutions Architect.</p>
+            </div>
+        </div> 
+
+        <div class="testimonial-card">
+            <img src="images/nosipho.jpg" alt="Michael Brown" class="testimonial-image"/>
+            <div class="testimonial-content">
+                <p class="testimonial-text">"The program was amazing, offering invaluable experiences that fueled growth and learning at every turn. It was a transformative journey filled with opportunities for personal and professional development."</p>
+                <p class="testimonial-author">- Nosipho Mncwango | Junior AWS Solutions Architect.</p>
+            </div>
+        </div> 
+    </div>
+
+        </>
+    )
 }
